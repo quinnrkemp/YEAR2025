5ceb3aa71aad7b34e8411442679c04bf1febfd87
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-02-28 22:20:22 -0500

    coding chall for 022825

---
+++ 022825.js
@@ -0,0 +1,10 @@
+    // ******MY ANSWER********
+
+    function multipleOfIndex(arr) {
+        const result = arr.filter((e,i) => e===0  || e%i===0);
+        
+        return result
+          // good luck
+        }
+        
+        // ***this is a re-do from yesterday's problem
