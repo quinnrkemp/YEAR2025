324bb3532e689f09b7d781d793f76feb8992272a
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-03-05 21:44:21 -0500

    coding chall for 030525

---
+++ 030525.js
@@ -0,0 +1,47 @@
+  // CHALLENGE NAME-  Abbreviate a Two Word Name
+
+    // DESCRIPTION:
+
+    // Write a function to convert a name into initials. This kata 
+    // strictly takes two words with one space in between them.
+
+    // The output should be two capital letters with a dot separating them.
+    
+    // It should look like this:
+    
+    // Sam Harris => S.H
+    
+    // patrick feeney => P.F
+    
+    
+
+    // // ***STARTER CODE***
+
+    // function abbrevName(name){
+    //     // code away
+    
+    // }
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Basic tests", () => {
+    //   it("Testing for fixed tests", () => {
+    //     assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+    //     assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+    //     assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+    //     assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+    //     assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function abbrevName(name){
+        return `${name.split(' ')[0][0]}.${name.split(' ')[1][0]}`.toUpperCase() 
+            // code away
+        
+        }
