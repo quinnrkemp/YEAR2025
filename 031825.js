e7ebe0bebd3c9247b99984ec811cefa9355678ae
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-03-18 21:25:47 -0400

    coding chall for 031825

---
+++ 031825.js
@@ -0,0 +1,46 @@
+  // CHALLENGE NAME- pick a set of first elements 
+
+
+    // DESCRIPTION:
+
+// Write a function to get the first element(s) of a sequence. 
+// Passing a parameter n (default=1) will return the first n 
+// element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+//     // ***STARTER CODE***
+
+//     function first(arr, n) {
+
+//         // return arr;
+//       }
+
+//     //   ******TEST CASES*****
+    
+//     const { assert } = require('chai');
+
+//     describe("Tests", () => {
+//       it("test", () => {
+//         const arr = ['a', 'b', 'c', 'd', 'e'];
+//         assert.sameOrderedMembers(first([...arr]   ),      ['a']);
+//         assert.sameOrderedMembers(first([...arr], 2), ['a', 'b']);
+//       });
+//     });
+
+    // ******MY ANSWER********
+
+function first(arr, n) {
+    if (n == 0) {
+        return arr = [];
+    } else if (!n) {
+        n = 1;
+    }
+    return arr.slice(0, n);
