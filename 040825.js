3003943a488b7c8fa73411c9ba44628411aac927
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-04-08 21:48:57 -0400

    coding chall for 040825

---
+++ 040825.js
@@ -0,0 +1,36 @@
+  // CHALLENGE NAME-  For Twins: 1. Types
+
+    // DESCRIPTION:
+
+    Task:
+    // Write a function that will accept two parameters: variable and 
+    // type and check if type of variable is matching type. Return true 
+    // if types match or false if not.
+    
+    // Examples:
+    // 42, "number"   --> true
+    // "42", "number" --> false
+
+    // // ***STARTER CODE***
+
+    // function typeValidation(variable, type) {
+    //     // Your code should be here ;) 
+    //   }   
+
+    // //   ******TEST CASES*****
+    
+    // const { assert } = require('chai');
+
+    // describe('Example tests', function() {
+    //   it("Basic Tests", function() {
+    //     assert.strictEqual(typeValidation(42, "number"), true);
+    //     assert.strictEqual(typeValidation("42", "number"), false);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function typeValidation(v, t) {
+        return typeof v===t
+         // Your code should be here ;) 
+       }
