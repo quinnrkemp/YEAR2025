08ecc118f40af7ca998a4c53fc38a564f3c1b76c
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-04-16 20:56:04 -0400

    coding chall for 041625

---
+++ 041625.js
@@ -0,0 +1,46 @@
+  // CHALLENGE NAME-  Removing Elements
+
+
+    // DESCRIPTION:
+
+    // Take an array and remove every second element from the array. 
+    // Always keep the first element and start removing with the next 
+    // element.
+
+    // Example:
+    // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+    
+    // None of the arrays will be empty, so you don't have to worry about that!
+
+    // // ***STARTER CODE***
+
+    // function removeEveryOther(arr){
+        
+    // }
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Basic tests",() =>{
+    //   it("Testing for fixed tests", () => {
+    //     assert.deepEqual(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+    //     assert.deepEqual(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+    //     assert.deepEqual(removeEveryOther([[1, 2]]), [[1, 2]]);
+    //     assert.deepEqual(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
+    //   })
+    // });
+
+    // ******MY ANSWER********
+
+function removeEveryOther(arr){
+    let res = []
+    for (let i=0; i<=arr.length-1; i+=2){
+      res.push(arr[i])
+    }
+    
+    return res
+      //your code here
+    }
