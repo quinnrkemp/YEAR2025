eea059cf252eea0b42e922dd86d9654454fff81d
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-04-19 14:58:56 -0400

    coding chall for 041925

---
+++ 041925.js
@@ -0,0 +1,40 @@
+  // CHALLENGE NAME-  Area or Perimeter
+
+    // DESCRIPTION:
+
+    // You are given the length and width of a 4-sided polygon. 
+    // The polygon can either be a rectangle or a square.
+    // If it is a square, return its area. If it is a rectangle, 
+    // return its perimeter.
+    
+    // Example(Input1, Input2 --> Output):
+    
+    // 6, 10 --> 32
+    // 3, 3 --> 9
+    // Note: for the purposes of this kata you will assume that it 
+    // is a square if its length and width are equal, otherwise it 
+    // is a rectangle.
+
+    // // ***STARTER CODE***
+
+    // const areaOrPerimeter = function(l , w) {
+    //      // Return your answer
+    //   };    
+
+    // //   ******TEST CASES*****
+    
+    // const { assert } = require('chai');
+
+    // describe("Tests", () => {
+    //   it("test", () => {
+    //     assert.strictEqual(areaOrPerimeter(3,  3),  9);
+    //     assert.strictEqual(areaOrPerimeter(6, 10), 32);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    const areaOrPerimeter = function(l , w) {
+        return !(l===w)?2*(l+w):l*w
+        // Return your answer
+      };
