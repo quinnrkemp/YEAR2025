9f34c16e50dcb3d5224f513e94b659098f6acfaf
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-04-25 16:45:42 -0400

    coding chall for 042525

--- 041825.js
+++ 041825.js
@@ -2,38 +2,38 @@
 
     // DESCRIPTION:
 
-    Given a set of numbers, return the additive inverse of each. 
-    Each positive becomes negatives, and the negatives become positives.
+    // Given a set of numbers, return the additive inverse of each. 
+    // Each positive becomes negatives, and the negatives become positives.
 
-    [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
-    [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
-    [] --> []
-    You can assume that all values are integers. Do not mutate the input 
-    array.
+    // [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+    // [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+    // [] --> []
+    // You can assume that all values are integers. Do not mutate the input 
+    // array.
 
-    // ***STARTER CODE***
+    // // ***STARTER CODE***
 
-    function invert(array) {
-        return ;
-     }   
+    // function invert(array) {
+    //     return ;
+    //  }   
 
-    //   ******TEST CASES*****
+    // //   ******TEST CASES*****
     
-    const Test = require('@codewars/test-compat');
+    // const Test = require('@codewars/test-compat');
 
-    const chai = require("chai");
-    const assert = chai.assert;
-    chai.config.truncateThreshold=0;
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
     
-    describe("Invert array values",() => {
-      const norm = arr => arr.map(n => n === -0 ? 0 : n);
-      it("Basic Tests", () => {
-        assert.deepEqual(norm(invert([1,2,3,4,5])), [-1,-2,-3,-4,-5]);
-        assert.deepEqual(norm(invert([1,-2,3,-4,5])), [-1,2,-3,4,-5]);
-        assert.deepEqual(norm(invert([])), []);
-        assert.deepEqual(norm(invert([0])), [0]);
-      });
-    });
+    // describe("Invert array values",() => {
+    //   const norm = arr => arr.map(n => n === -0 ? 0 : n);
+    //   it("Basic Tests", () => {
+    //     assert.deepEqual(norm(invert([1,2,3,4,5])), [-1,-2,-3,-4,-5]);
+    //     assert.deepEqual(norm(invert([1,-2,3,-4,5])), [-1,2,-3,4,-5]);
+    //     assert.deepEqual(norm(invert([])), []);
+    //     assert.deepEqual(norm(invert([0])), [0]);
+    //   });
+    // });
 
     // ******MY ANSWER********
 
