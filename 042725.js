9b9158e6bcf872f4ae44efea36338576861cf8cf
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-04-27 20:51:29 -0400

    coding chall for 042725

---
+++ 042725.js
@@ -0,0 +1,40 @@
+  // CHALLENGE NAME-  Swap Values
+
+    // DESCRIPTION:
+
+    // I would like to be able to pass an array with two elements 
+    // to my swapValues function to swap the values. However it 
+    // appears that the values aren't changing.
+
+    // Can you figure out what's wrong here?
+
+    // // ***STARTER CODE***
+
+    // function swapValues() {
+    //     var args = Array.prototype.slice.call(arguments);
+    //     var temp = args[0];
+    //     args[0] = args[1];
+    //     args[1] = temp;
+    // }   
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Tests", () => {
+    //   it("test", () => {
+    // var arr = [1,2] 
+    // swapValues(arr);
+    // Test.assertEquals(arr[0], 2, "Failed swapping numbers")
+    // Test.assertEquals(arr[1], 1, "Failed swapping numbers")
+    
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function swapValues(args) {
+        var temp = args[0];
+        args[0] = args[1];
+        args[1] = temp;
+      }
