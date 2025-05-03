1688d69e50c1f2bc6b67b7dc652d29b270ea6981
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-03 16:03:04 -0400

    coding chall for 050325

---
+++ 050325.js
@@ -0,0 +1,21 @@
+  // CHALLENGE NAME-  practice coding from yesterdays challenge
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+function addLength(str) {
+    return str.split(' ').map((x)=> `${x} ${x.length}`)
+      
+      //start-here
+    }
