d3602383000b9b5c3fabcbf991e39bdc55db5de6
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-06 18:10:46 -0400

    coding chall for 050625

---
+++ 050625.js
@@ -0,0 +1,34 @@
+  // CHALLENGE NAME-  You Can't Code Under Pressure #1
+
+
+    // DESCRIPTION:
+
+    // Code as fast as you can! You need to double the integer and return it.
+
+    // // ***STARTER CODE***
+
+    // function doubleInteger(i) {
+
+    //     // i will be an integer. Double it and return it.
+    //   //   return 4*i/2;
+    //     return i
+    //   }   
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Tests", () => {
+    //   it("test", () => {
+    // Test.assertEquals(doubleInteger(2), 4);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function doubleInteger(i) {
+
+        // i will be an integer. Double it and return it.
+      //   return 4*i/2;
+        return i*=2
+      }
