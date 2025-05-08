fae7634d61cf823c276e6b76489f532541a37e5c
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-07 22:26:18 -0400

    coding chall for 050725

---
+++ 050725.js
@@ -0,0 +1,33 @@
+  // CHALLENGE NAME-  Multiply
+
+    // DESCRIPTION:
+
+    // This code does not execute properly. Try to figure out why.
+
+    // // ***STARTER CODE***
+
+    // function multiply(a, b){
+    //     a * b
+    //   } 
+
+    // //   ******TEST CASES*****
+    
+    // const assert = require("chai").assert;
+
+    // describe("Multiply", () => {
+    //   it("fixed tests", () => {
+    //     assert.strictEqual(multiply(1,1), 1);
+    //     assert.strictEqual(multiply(2,1), 2);
+    //     assert.strictEqual(multiply(2,2), 4);
+    //     assert.strictEqual(multiply(3,5), 15); 
+    //     assert.strictEqual(multiply(5,0), 0);
+    //     assert.strictEqual(multiply(0,5), 0);
+    //     assert.strictEqual(multiply(0,0), 0); 
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function multiply(a, b){
+        return a * b
+      }
