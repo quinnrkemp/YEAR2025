3002b8f7e491e32342dca767351d33536bb3be87
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-08 22:03:24 -0400

    coding chall for 050825

---
+++ 050825.js
@@ -0,0 +1,34 @@
+  // CHALLENGE NAME-  Square(n)Sum
+
+    // DESCRIPTION:
+
+    // Complete the square sum function so that it squares each 
+    // number passed into it and then sums the results together.
+
+    // For example, for [1, 2, 2] it should return 9 because 1**2 + 2**2 + 2**2 =9.  
+
+    // // ***STARTER CODE***
+
+    // function squareSum(numbers){
+
+    // }   
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Tests", () => {
+    //   it("test", () => {
+    //     assert.strictEqual(squareSum([1,2]), 5);
+    //     assert.strictEqual(squareSum([0, 3, 4, 5]), 50);
+    //     assert.strictEqual(squareSum([]), 0);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function squareSum(n){
+        return n.map((a)=>a**2).reduce((a,b)=>a+b,0)
+        }
