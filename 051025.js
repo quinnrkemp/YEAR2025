497b923474bbf73ff7d3451d0d3d380e05a60fd7
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-10 21:56:19 -0400

    coding chall for 051025

---
+++ 051025.js
@@ -0,0 +1,45 @@
+  // CHALLENGE NAME-  Grasshopper - Array Mean
+
+    // DESCRIPTION:
+
+    // Find Mean
+    // Find the mean (average) of a list of numbers in an array.
+    
+    // Information
+    // To find the mean (average) of a set of numbers add all of the 
+    // numbers together and divide by the number of values in the list.
+    
+    // For an example list of 1, 3, 5, 7
+    
+    // 1. Add all of the numbers
+    
+    // 1+3+5+7 = 16
+    // 2. Divide by the number of values in the list. In this example 
+    // there are 4 numbers in the list.
+    
+    // 16/4 = 4
+    // 3. The mean (or average) of this list is 4
+
+    // // ***STARTER CODE***
+
+    // function findAverage(nums) {
+    //     // Code here
+    //   }   
+
+    // //   ******TEST CASES*****
+    
+    // const {assert} = require('chai');
+
+    // describe('Get average', function () {
+    //   it('Should return the average', function () {
+    //     assert.approximately(findAverage([1]), 1, 1e-9);
+    //     assert.approximately(findAverage([1, 3, 5, 7]), 4, 1e-9);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function findAverage(n) {
+        return n.reduce((a,b)=>a+b)/n.length
+          // Code here
+        } 
