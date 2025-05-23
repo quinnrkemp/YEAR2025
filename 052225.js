08ef1ff9a27822758be9ac8933c28c4417e95d38
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-22 21:10:31 -0400

    coding chall for 052225

---
+++ 052225.js
@@ -0,0 +1,55 @@
+  // CHALLENGE NAME-  Opposite Number
+
+    // DESCRIPTION:
+
+    // Very simple, given a number (integer / decimal / both 
+    //     depending on the language), find its opposite (additive 
+    //         inverse).
+
+    // Examples:
+    
+    // 1: -1
+    // 14: -14
+    // -34: 34
+
+    // // ***STARTER CODE***
+
+    // function opposite(number) {
+ 
+    //     //your code here
+    //   }    
+
+    // //   ******TEST CASES*****
+    
+    // const { assert } = require('chai');
+
+    // describe('Fixed tests', () => {
+    //   it('Is it a function?', () => {
+    //     assert.strictEqual(typeof opposite, 'function', 'opposite should be a function');
+    //   });
+    //   it('should return -1', () => {
+    //     assert.strictEqual(opposite(1), -1);
+    //   });
+    //   it('should return 0', () => {
+    //     assert.strictEqual(opposite(0), 0);
+    //   });
+    //   it('should return -4.25', () => {
+    //     assert.strictEqual(opposite(4.25), -4.25);
+    //   });
+    //   it('should return -3.3333333', () => {
+    //     assert.strictEqual(opposite(3.3333333), -3.3333333);
+    //   });
+    //   it('should return 12525220.3325', () => {
+    //     assert.strictEqual(opposite(-12525220.3325), 12525220.3325);
+    //   });
+    //   it('should return 5', () => {
+    //     assert.strictEqual(opposite(-5), 5);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function opposite(n) {
+        return -n
+          //your code here
+        }
