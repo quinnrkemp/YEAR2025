4398664ec2e9e3874bc0966dd2a6401f13a9ae12
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-27 19:48:29 -0400

    coding chall for 052625

---
+++ 052725.js
@@ -0,0 +1,39 @@
+// CHALLENGE NAME-  Third Angle of a Triangle   (#2)
+
+// DESCRIPTION:
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+
+// // ***STARTER CODE***
+
+// function otherAngle(a, b) {
+//     return 0;
+//   }
+
+// //   ******TEST CASES*****
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(otherAngle(30, 60), 90);
+//     assert.strictEqual(otherAngle(60, 60), 60);
+//     assert.strictEqual(otherAngle(43, 78), 59);
+//     assert.strictEqual(otherAngle(10, 20), 150);
+//   });
+// });
+
+// ******MY ANSWER********
+
+function otherAngle(a, b) {
+    return 180-(a+b)
+  }
