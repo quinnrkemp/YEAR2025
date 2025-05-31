7345961b7e6b56f12c9672ee9e9123ba9aa2fc75
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-05-31 16:27:10 -0400

    coding chall for 053125

---
+++ 053125.js
@@ -0,0 +1,28 @@
+  // CHALLENGE NAME-  If you can't sleep, just count sheep!! 
+
+            // *****(PRACTICE FROM YESTERDAYS CODING CHALLENGE)*****
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+    function countSheep(num){
+        let res= []
+        for (let i=1; i<=num; i++) {
+         res.push(`${i} sheep...`)
+       //    res.push(num)
+        } 
+         return res.join('')
+         //your code here
+       }
+
