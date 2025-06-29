dcd4792fa29d1c003eacbcb4f547860a2bfd8802
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-06-28 20:41:08 -0400

    coding chall for 062825

---
+++ 062825.js
@@ -0,0 +1,39 @@
+  // CHALLENGE NAME-  Template Strings
+
+    // DESCRIPTION:
+
+    // Task
+    // Your task is to return the correct string using the Template String Feature.
+    // Input
+    // Two Strings, no validation is needed.
+    // Output
+    // You must output a string containing the two strings with the word ```' are '```
+
+    // // ***STARTER CODE***
+
+    // var templateStrings = function(noun, adjective) {
+    //     return //complete this
+    //   }   
+
+    // //   ******TEST CASES*****
+    
+    // describe('Testing templateStrings', () => {
+    //     const {assert} = require('chai')
+        
+    //     const doTest = (noun, adjective, expected, clue) => {
+    //       const actual = templateStrings(noun, adjective)
+    //       assert.strictEqual(actual, expected, clue)
+    //     }
+        
+    //     it('Basic tests', () => {
+    //       doTest('Animals', 'Good', 'Animals are Good', 'templateStrings(\'Animals\', \'Good\')')
+    //       doTest('You', 'special', 'You are special', 'templateStrings(\'You\', \'special\')')
+    //       doTest('lives', 'frozen', 'lives are frozen', 'templateStrings(\'lives\', \'frozen\')')
+    //     })
+    //   })
+
+    // ******MY ANSWER********
+
+    var templateStrings = function(n, a) {
+        return `${n} are ${a}`
+      }
