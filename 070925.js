9dd425d927bc4657791382079b37803985b93932
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-07-09 23:01:06 -0400

    coding chall for 070925

---
+++ 070925.js
@@ -0,0 +1,56 @@
+  // CHALLENGE NAME-  Remove First and Last Character Part Two
+
+    // DESCRIPTION:
+
+    // You are given a string containing a sequence of character sequences 
+    // separated by commas.
+
+    // Write a function which returns a new string containing the same 
+    // character sequences except the first and the last ones but this 
+    // time separated by spaces.
+    
+    // If the input string is empty or the removal of the first and last 
+    // items would cause the resulting string to be empty, return an empty 
+    // value (represented as a generic value NULL in the examples below).
+    
+    // Examples
+    // "1,2,3"      =>  "2"
+    // "1,2,3,4"    =>  "2 3"
+    // "1,2,3,4,5"  =>  "2 3 4"
+    
+    // ""     =>  NULL
+    // "1"    =>  NULL
+    // "1,2"  =>  NULL
+
+    // // ***STARTER CODE***
+
+    // function array(string) {
+    //     // TODO
+    //   }   
+
+    // //   ******TEST CASES*****
+    
+    // const {assert} = require('chai');
+
+    // describe('Sample Tests', function() {
+    //   function test(string, expected) {
+    //     it(`string='${string}'`, function() {
+    //       assert.strictEqual(array(string), expected);
+    //     });
+    //   }
+    
+    //   test('', null);
+    //   test('1', null);
+    //   test('A1,B2', null);
+    //   test('1,2,3', '2');
+    //   test('1,2,3,4', '2 3');
+    //   test('A1,B2,C3,D4,E5', 'B2 C3 D4');
+    //   test('A,1,23,456,78,9,Z', '1 23 456 78 9');
+    // });
+
+    // ******MY ANSWER********
+
+    function array(s) {
+        return s.split(',').slice(1,-1).join(' ')||null
+         // TODO
+       }
