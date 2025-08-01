a67090dd942e9b0968112b2dd1bfb3d2124fd183
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-07-31 22:05:53 -0400

    coding chall for 073125

---
+++ 073125.js
@@ -0,0 +1,49 @@
+  // CHALLENGE NAME-  All Star Code Challenge #18
+
+
+    // DESCRIPTION:
+
+    // Create a function that accepts a string and a single character, 
+    // and returns an integer of the count of occurrences the 2nd 
+    // argument is found in the first one.
+
+    // If no occurrences can be found, a count of 0 should be returned.
+    
+    // ("Hello", 'o')  =>  1
+    // ("Hello", 'l')  =>  2
+    // ("", 'z')       =>  0
+    // Notes
+    // The first argument can be an empty string
+    // In languages with no distinct character data type, the second argument 
+    // will be a string of length 1
+
+    // // ***STARTER CODE***
+
+    // function strCount(str, letter){  
+
+    //     //code here
+    //   }    
+
+    // //   ******TEST CASES*****
+    
+    // const { assert } = require('chai');
+
+    // describe("Tests", () => {
+    //   it("test", () => {
+    //     assert.strictEqual(strCount('Hello', 'o'), 1);
+    //     assert.strictEqual(strCount('Hello', 'l'), 2);
+    //     assert.strictEqual(strCount('',      'z'), 0);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function strCount(str, l){  
+        // let res=[]
+        // for (let i=0; i<=str.length ; i++){ 
+        //  res.push(i===l)
+          return [...str].filter((e)=> e===l).length
+          //code here
+        }
+        // return res
+        // }
