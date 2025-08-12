6c1ae0c569ba050fd7656c7934d4503cfb0c1cd2
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-08-11 21:15:05 -0400

    coding chall for 081125

---
+++ 081125.js
@@ -0,0 +1,39 @@
+  // CHALLENGE NAME-  Convert a string to an array  
+
+    // DESCRIPTION:
+
+    // Write a function to split a string and convert it into an array of words.
+
+    // Examples (Input ==> Output):
+    // "Robin Singh" ==> ["Robin", "Singh"]
+    
+    // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+    // // ***STARTER CODE***
+
+    // function stringToArray(string){
+
+    //     // code code code
+    
+    // }   
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Basic tests", () => {
+    //   it("Testing for fixed tests", () => {
+    //     assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+    //     assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function stringToArray(s){
+        return s.split(' ')
+            // code code code
+        
+        }
