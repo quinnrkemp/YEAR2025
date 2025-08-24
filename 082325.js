d9853cdc003b99b5610e9fb4a5e40902838e7ca2
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-08-23 22:06:18 -0400

    coding chall for 082325

---
+++ 082325.js
@@ -0,0 +1,25 @@
+// CHALLENGE NAME-  Crash Override  ***practice #3***
+
+function aliasGen(first, last){
+    let fi= first[0].toUpperCase()
+    let la= last[0].toUpperCase()
+    return fi===`${+fi}` || la===`${+la}`? 'Your name must start with a letter from A - Z.': firstName[fi] + ' ' + surname[la]
+      // Code Here
+    }
+
+// *****Object practice below***** 
+
+// class MakeCar {
+//         constructor(carMake, carModel, carColor, carDoors){
+//             this.make=carMake
+//             this.model=carModel
+//             this.color=carColor
+//             this.color=carDoors
+//         }
+//         honkHorn(){
+//             console.log('aaahuugaah')
+//         }
+//     }
+//     let hondaVolt= new MakeCar('Honda', 'Volt', 'white', 3)
+//     // console.log (hondaVolt)
+    
