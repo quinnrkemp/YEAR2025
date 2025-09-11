f06df6fea08c33a00966d010e15cde2101e84b8c
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-09-11 15:57:08 -0400

    coding chall for 091125

---
+++ 091125.js
@@ -0,0 +1,36 @@
+  // CHALLENGE NAME-  Return Strings
+
+    // DESCRIPTION:
+
+    // Create a function that accepts a parameter representing a 
+    // name and returns the message: "Hello, <name> how are you 
+    // doing today?".
+
+    // [Make sure you type the exact thing I wrote or the program 
+    //     may not execute properly]
+
+    // // ***STARTER CODE***
+
+    // function greet(name){
+    //     //your code here
+    //   }    
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Basic tests",() =>{
+    //   it("Testing for fixed tests", () => {
+    //     assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+    //     assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+    //   })
+    // })
+
+    // ******MY ANSWER********
+
+    function greet(n){
+        return `Hello, ${n} how are you doing today?`
+          //your code here
+        }
