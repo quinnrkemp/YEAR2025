f3d3f8087c8d51b071565c7aa65020456214d26a
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-09-12 15:55:38 -0400

    coding chall for 091225

---
+++ 091225.js
@@ -0,0 +1,45 @@
+  // CHALLENGE NAME-  Template Strings
+
+    // DESCRIPTION:
+
+    // Template Strings
+    // Template Strings, this kata is mainly aimed at the new JS ES6 
+    // Update introducing Template Strings
+    // Task
+    // Your task is to return the correct string using the Template 
+    // String Feature.
+    // Input
+    // Two Strings, no validation is needed.
+    // Output
+    // You must output a string containing the two strings with the word ```' are '```
+
+    // // ***STARTER CODE***
+
+    // var templateStrings = function(noun, adjective) {
+        
+    //    //   return //complete this
+    //    }   
+
+    // //   ******TEST CASES*****
+    
+    // describe('Testing templateStrings', () => {
+    //     const {assert} = require('chai')
+        
+    //     const doTest = (noun, adjective, expected, clue) => {
+    //       const actual = templateStrings(noun, adjective)
+    //       assert.strictEqual(actual, expected, clue)
+    //     }
+        
+    //     it('Basic tests', () => {
+    //       doTest('Animals', 'Good', 'Animals are Good', 'templateStrings(\'Animals\', \'Good\')')
+    //       doTest('You', 'special', 'You are special', 'templateStrings(\'You\', \'special\')')
+    //       doTest('lives', 'frozen', 'lives are frozen', 'templateStrings(\'lives\', \'frozen\')')
+    //     })
+    //   })
+
+    // ******MY ANSWER********
+
+    var templateStrings = function(n, a) {
+        return `${n} are ${a}`
+       //   return //complete this
+       }
