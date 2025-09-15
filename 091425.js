c8621e6c34c36b17f7630e339281891f3ce82b4f
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-09-14 20:59:45 -0400

    coding chall for 091425

---
+++ 091425.js
@@ -0,0 +1,31 @@
+  // CHALLENGE NAME-  Convert a Boolean to a String
+
+    // DESCRIPTION:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//     // ***STARTER CODE***
+
+//  function booleanToString(b){
+//  //your code here
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+// Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+//   });
+// });
+
+    // ******MY ANSWER********
+
+    function booleanToString(b){
+ return `${b}` 
+ //your code here
+}
