d1c0ee64ef14a35cf99f27ac55821f895fa208aa
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-10-30 19:19:48 -0400

    coding chall for 103025

---
+++ 103025.js
@@ -0,0 +1,21 @@
+  // CHALLENGE NAME-  Rock, Paper, Scissors practice
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+  function rockPaperScissors(my){
+    let bot= Math.random()
+        return bot<.33?`rock`: bot>.66?`scissors`:`paper`
+
+  }  
