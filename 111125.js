8169264d9935a467cd2d299c3687c350f82ff68b
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-11-11 15:48:57 -0500

    coding chall for 111125

---
+++ 111125.js
@@ -0,0 +1,41 @@
+  // CHALLENGE NAME-  Potenciation
+
+    // DESCRIPTION:
+
+// Your task is to implement a function that takes two inputs, 
+// x and y, and returns the value of x raised to the power of y.
+
+// Simple, right? But you are NOT allowed to use Math.pow();
+
+// Both x and y will be natural numbers (non-negative integers) in 
+// range 0 <= x,y <= 13.
+
+// Special Case: For this challenge, 0^0 is defined to be 1.
+
+//     // ***STARTER CODE***
+
+// function power(x,y){
+
+//   //SHOW ME WHAT YOU GOT!
+// }    
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+
+// // JS tests are using Mocha. You can use `chai` for assertions.
+// describe("Example tests", function(){
+//   it("Examples", function(){
+//     assert.strictEqual(power(1, 701270), 1);
+//     assert.strictEqual(power(2, 2), 4);
+//     assert.strictEqual(power(3, 2), 9);
+//     assert.strictEqual(power(-1, 40), 1);  
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function power(x,y){
+return x**y
+  //SHOW ME WHAT YOU GOT!
+}
