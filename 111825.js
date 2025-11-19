513927b21f51e3c97dbd33a1d83483fd4823f594
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-11-18 19:02:07 -0500

    coding chall for 111825

---
+++ 111825.js
@@ -0,0 +1,54 @@
+  // CHALLENGE NAME-  Contamination #1 -String
+
+    // DESCRIPTION:
+
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// Starting with the original text, and given a character, return 
+// the text once it has been mutated so that all of the characters 
+// in the original text have been replaced with the character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+//     // ***STARTER CODE***
+
+// function contamination(text, char){
+//   // Code here ;)
+// }    
+
+//     //   ******TEST CASES*****
+    
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+
+// describe("Sample tests", () => {
+//   it("Should pass sample tests", () => {
+//     assert.strictEqual(contamination("abc","z"), "zzz")
+//     assert.strictEqual(contamination("","z"), "")
+//     assert.strictEqual(contamination("abc",""), "")
+//     assert.strictEqual(contamination("_3ebzgh4","&"), "&&&&&&&&")
+//     assert.strictEqual(contamination("//case"," "), "      ")
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function contamination(t, c){
+
+  return [...t].map((a)=>c).join('')
+//  have to use spread syntax above instead of just '[t]'...otherwise the 
+//   array will print as ['abc'] instead of ['a','b','c']
+  
+}
