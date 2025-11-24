9c0c907828c579bfac5e6071371a358b91ab8907
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-11-23 19:50:11 -0500

    coding chall for 112325

---
+++ 112325.js
@@ -0,0 +1,31 @@
+  // CHALLENGE NAME-  5 without numbers!!
+
+    // DESCRIPTION:
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any 
+// of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+//     // ***STARTER CODE***
+
+//  function unusualFive() {
+//   return 5;
+// }   
+
+//     //   ******TEST CASES*****
+    
+// describe("unusualFive", function(){
+//   it("should return 5", function(){
+//     assert.strictEqual(unusualFive(), 5);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+  function unusualFive() {
+  let arr=['a','b','c','d','e'] 
+  return Math.pow(arr.length,['a'].length)
+  }
