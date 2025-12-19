a63f13ad43f53e60a91ce6a2c54a0c6a3cad8aa5
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-12-18 19:05:44 -0500

    coding chall for 121825

---
+++ 121825.js
@@ -0,0 +1,35 @@
+  // CHALLENGE NAME-  Simple multiplication
+
+    // DESCRIPTION:
+
+// This kata is about multiplying a given number by eight 
+// if it is an even number and by nine otherwise.
+
+//     // ***STARTER CODE***
+
+//  function simpleMultiplication(number) {
+//     // your code........
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic Tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(simpleMultiplication(2),16,'Should return given argument times eight...')
+//     assert.strictEqual(simpleMultiplication(1),9, 'Should return given argument times nine...')
+//     assert.strictEqual(simpleMultiplication(8),64,'Should return given argument times eight...')
+//     assert.strictEqual(simpleMultiplication(4),32,'Should return given argument times eight...')
+//     assert.strictEqual(simpleMultiplication(5),45,'Should return given argument times nine...')
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function simpleMultiplication(n) {
+return n%2===0?n*8:n*9
+  // your code........
+}
