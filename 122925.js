a66ae20d82202ee825ef06cf76e7e1a99d62ac65
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2025-12-29 17:39:09 -0500

    coding chall for 122925

---
+++ 122925.js
@@ -0,0 +1,32 @@
+  // CHALLENGE NAME-  Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+
+    // DESCRIPTION:
+
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// "Hi!" --> "H!!"
+// "!Hi! Hi!" --> "!H!! H!!"
+// "aeiou" --> "!!!!!"
+// "ABCDE" --> "!BCD!"
+
+
+//     // ***STARTER CODE***
+
+//  function replace(s) {
+//   //coding and coding....
+//     return '';
+// }   
+
+//     //   ******TEST CASES*****
+    
+// function replace(s) {
+//   //coding and coding....
+//     return '';
+// }
+
+    // ******MY ANSWER********
+
+replace=s=>s.replace(/[aeiou]/gi, '!')
+
+// ***not my answer
