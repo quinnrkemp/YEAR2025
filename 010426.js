e541b2b58119ea2424273b718426ea8948c02757
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-01-04 19:59:10 -0500

    coding chall for 010426

---
+++ 010426.js
@@ -0,0 +1,39 @@
+  // CHALLENGE NAME-  Simple Comparison?
+
+    // DESCRIPTION:
+
+// Write a function that will compare two values, one will be a 
+// number and one will be a string. Return true if they are the 
+// same character (regardless of their different data types) and 
+// return false if they are not.
+
+// To make this challange harder and to promp the challenger to 
+// read up about coercion I have disabled some of the built in 
+// methods including .toString(), .join(), .split(), parseInt 
+// and .Number().
+
+//     // ***STARTER CODE***
+
+//  function add(a, b){
+// 	// code here
+// }   
+
+//     //   ******TEST CASES*****
+    
+// describe("Tests", () => {
+//   it("test", () => {
+//  Test.assertEquals(add('1', 1), true);
+//  Test.assertEquals(add(1, '1'), true);
+//  Test.assertEquals(add(1, '0'), false);
+//  Test.assertEquals(add('11', 11), true);
+//  Test.assertEquals(add(12, 12), true);
+//  Test.assertEquals(add(120, '021'), false);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function add(a, b){
+return a==b
+  // code here
+}
