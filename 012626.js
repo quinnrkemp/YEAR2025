52d3a2ef216ccd4a2dee9cae2cdd0d1bb4da840f
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-01-26 18:37:25 -0500

    coding chall for 012626

---
+++ 012626.js
@@ -0,0 +1,44 @@
+  // CHALLENGE NAME-  Training JS #1: create your first JS function and print "Hello World!" 
+
+    // DESCRIPTION:
+
+// Please refer to two example above and write your first JS function.
+
+// mission 1:
+
+// use keyword function to define your function, function name should be helloWorld(don't 
+// forget the () and {})
+
+// mission 2:
+
+// use keyword var (or let or const) to define a variable str, value of str should be a 
+// string: "Hello World!"(don't forget the =).
+
+// mission 3:
+
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+//     // ***STARTER CODE***
+
+//   no starter code for this kata  
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.isFunction(helloWorld, "function helloWorld is not defined")
+//     helloWorld();
+//   });
+// });
+
+    // ******MY ANSWER********
+
+  function helloWorld(){
+  let str= "Hello World!"
+  console.log(str)
+}
