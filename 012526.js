ef23b589ad09a7b6fa91b9670ed4c3abb62199f3
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-01-25 19:11:48 -0500

    coding chall for 012526

---
+++ 012526.js
@@ -0,0 +1,43 @@
+  // CHALLENGE NAME-  If you can't sleep, count sheep!!
+
+    // DESCRIPTION:
+
+// If you can't sleep, just count sheeps!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: 
+// "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative 
+// integers.
+
+//     // ***STARTER CODE***
+
+// var countSheep = function (num){
+//   //your code here
+// }    
+
+//     //   ******TEST CASES*****
+    
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Fixed tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(countSheep(0), "");
+//     assert.strictEqual(countSheep(1), "1 sheep...");
+//     assert.strictEqual(countSheep(2), "1 sheep...2 sheep...");
+//     assert.strictEqual(countSheep(3), "1 sheep...2 sheep...3 sheep...");
+//   });
+// });
+
+    // ******MY ANSWER********
+
+var countSheep = function (num){
+ let res=''
+ for (let i=1; i<=num; i++){
+  res += `${i} sheep...` 
+ }
+ return res 
+
+  //your code here
+}
