7052e5fd3b7a12cf3914591b8e56e2fd60bac3cd
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-01-27 19:14:55 -0500

    coding chall for 012726

---
+++ 012726.js
@@ -0,0 +1,39 @@
+  // CHALLENGE NAME-  Bin to Decimal
+
+    // DESCRIPTION:
+
+// Complete the function which converts a binary number (given as a string) 
+// to a decimal number.
+
+//     // ***STARTER CODE***
+
+// function binToDec(bin) {
+ 
+//   // TODO
+// }    
+
+//     //   ******TEST CASES*****
+    
+// const {assert} = require('chai');
+
+// describe('Sample Tests', function() {
+//   const tests = [
+//     ['0', 0],
+//     ['1', 1],
+//     ['10', 2],
+//     ['1001001', 73],
+//   ];
+
+//   for (let [input, expected] of tests) {
+//     it(`bin = '${input}'`, function() {
+//       assert.strictEqual(binToDec(input), expected);
+//     });
+//   }
+// });
+
+    // ******MY ANSWER********
+
+function binToDec(bin) {
+return parseInt(bin,2) 
+  // TODO
+}
