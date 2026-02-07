ce4f2a406ca5172573c38805967f2d4b2340c14e
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-02-06 22:31:12 -0500

    coding chall for 020626

---
+++ 020626.js
@@ -0,0 +1,31 @@
+  // CHALLENGE NAME-  MakeUpperCase
+
+    // DESCRIPTION:
+
+// Write a function which converts the input string to uppercase.
+
+//     // ***STARTER CODE***
+
+//  function makeUpperCase(str) {
+//   // Code here
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+
+// describe("Basic Tests", function(){
+//   it("should pass the basic tests", function(){
+//     assert.strictEqual(makeUpperCase(""),           "");
+//     assert.strictEqual(makeUpperCase("hello"), "HELLO");
+//     assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+//     assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function makeUpperCase(s) {
+return s.toUpperCase()
+  // Code here
+}
