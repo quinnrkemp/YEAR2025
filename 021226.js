f639d6d0f24efb1ff88658267be0a63d83770358
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-02-12 21:58:35 -0500

    coding chall for 021226

--- 021226.js
+++ 021226.js
@@ -2,13 +2,13 @@
 
     // DESCRIPTION:
 
-The Task
-Think of a way to store the languages as a database. The languages are 
-listed below so you can copy and paste!
-Write a 'welcome' function that takes a parameter 'language', with a type 
-String, and returns a greeting - if you have it in your database. It should 
-default to English if the language is not in the database, or in the event 
-of an invalid input.
+// The Task
+// Think of a way to store the languages as a database. The languages are 
+// listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language', with a type 
+// String, and returns a greeting - if you have it in your database. It should 
+// default to English if the language is not in the database, or in the event 
+// of an invalid input.
 
     // ***STARTER CODE***
 
