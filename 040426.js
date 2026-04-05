9a5f3886b3927a8d7aaff18dbb5cf1a4c3e94234
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-04-04 21:43:15 -0400

    coding chall for 040426

---
+++ 040426.js
@@ -0,0 +1,29 @@
+  // CHALLENGE NAME-  Chuck Norris VII - True or False? (Beginner)
+
+    // DESCRIPTION:
+
+// Your task is to be more like Chuck (ha! good luck!). You must return false without
+//  ever actually using the word false...
+
+//     // ***STARTER CODE***
+
+//  function ifChuckSaysSo(){
+
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+
+// describe("Example tests", () => {
+//   it("Only one test", () => {
+//     assert.strictEqual(ifChuckSaysSo(), false);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function ifChuckSaysSo(){
+return 5==true
+//   return [1,2,3,5,7].length===3
+}
