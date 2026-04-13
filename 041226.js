1a9132c16eedfec49c2598b1d272ebfd018d2cb3
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-04-12 21:58:22 -0400

    coding chall for 041226

---
+++ 041226.js
@@ -0,0 +1,32 @@
+// CHALLENGE NAME-  practice #2 from yesterday's lesson (objects)
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+class Person {
+constructor (firstName="John", lastName="Doe", age=0, gender="Male") {
+  this.firstName= firstName
+  this.lastName= lastName
+  this.age= age
+  this.gender= gender
+}
+  sayFullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+  static greetExtraTerrestrials(raceName){
+    return `Welcome to Planet Earth ${raceName}`
+  }
+  
+  // Get coding in ES6 :D
+}
