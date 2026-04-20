f1fd585222ed5b182b971d608bd01e5616cb018d
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-04-20 19:50:59 -0400

    coding chall for 042026

---
+++ 042026.js
@@ -0,0 +1,32 @@
+  // CHALLENGE NAME-  Grasshopper - Function syntax debugging
+
+    // DESCRIPTION:
+
+// A student was working on a function and made some syntax mistakes while 
+// coding. Help them find their mistakes and fix them.
+
+//     // ***STARTER CODE***
+
+//  function main [verb, noun]
+//   return verb + noun
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.isFunction(main)
+//     assert.strictEqual(main('take ', 'item'), 'take item')
+//     assert.strictEqual(main('use ', 'sword'), 'use sword')
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function main(verb,noun){ 
+  return `${verb}${noun}`
+// ***alt answer below***
+  //   return verb + noun
+}
