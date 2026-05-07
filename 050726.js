823e8646f2ea37593174c9d660bddd373e802b8b
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-05-07 12:33:32 -0400

    coding chall for 050726

---
+++ 050726.js
@@ -0,0 +1,22 @@
+  // CHALLENGE NAME-  practice problem
+
+    // DESCRIPTION:
+
+// create a function that takes in an array of numbers. 
+// multiply each number together and alert the product
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+function multiNums(arr){
+    console.log(arr.reduce((a,b)=>a*b,1))
+}
+
+multiNums([10,5,3])
