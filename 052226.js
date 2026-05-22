18d821d234ed27093e69ce0438d2280fedddbcd2
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-05-22 01:23:29 -0400

    coding chall for 052226

---
+++ 052226.js
@@ -0,0 +1,45 @@
+  // CHALLENGE NAME-  Drink about
+
+    // DESCRIPTION:
+
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+
+//     // ***STARTER CODE***
+
+//  function peopleWithAgeDrink(o) {
+// };   
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require("chai")
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(peopleWithAgeDrink(22), 'drink whisky');
+//     })
+//   })
+
+    // ******MY ANSWER********
+
+function peopleWithAgeDrink(o) {
+  return o>=21?'drink whisky':o<21&&o>17?'drink beer':o<18&&o>13?'drink coke':'drink toddy'
+};
