0a9380eadeb6dd2d0cba93ae861fd7bc2dbd2ce5
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-05-30 22:09:58 -0400

    coding chall for 053026

---
+++ 053026.js
@@ -0,0 +1,28 @@
+    // CHALLENGE NAME-  practice problem
+
+    // DESCRIPTION:
+
+// print out all characters from the string 'asap frontend'
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+ function oddEvenDiv5(n){
+let res=[]
+    for (let i=0; i<=n.length-1; i++){
+//  res.push(i%5===0?'five':i%2===0?'even':'odd')
+//   res.push(i%5===0?`${i} five`:i%2===0?`${i} even`:`${i} odd`)
+ res.push(n[i])
+//  console.log(i)
+    }
+console.log(res)
+
+}
+oddEvenDiv5('asap frontend')   
