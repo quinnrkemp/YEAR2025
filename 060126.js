a9884e7e04858f0466331bd8c18a3d4f59328302
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-06-01 20:38:12 -0400

    coding chall for 060126

---
+++ 060126.js
@@ -0,0 +1,24 @@
+    // CHALLENGE NAME-  practice problem
+
+    // DESCRIPTION:
+
+// create a function that converts $usd into $aud
+
+// aud to usd formula= usd X 1.5
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+function audUsd (usd){
+console.log( usd * 1.5)
+}
+audUsd(1000)
+audUsd(500)
+audUsd(200)
