8f97336eae27f6685a15722d77280b933f2ef134
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-06-07 00:53:40 -0400

    coding chall for 060726

---
+++ 060726.js
@@ -0,0 +1,25 @@
+    // CHALLENGE NAME-  Function 2- Squaring and argument
+
+    // DESCRIPTION:
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//     // ***STARTER CODE***
+
+//   no starter code  
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(square(3), 9);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function square (num) {
+  return num *num
+}
