a7c9c904c8f9cc7f00a2c420e98840147ae537fc
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-07-20 21:19:18 -0400

    coding chall for 072026

---
+++ 072026.js
@@ -0,0 +1,41 @@
+    // CHALLENGE NAME-  How many lightsabers do you own?
+
+    // DESCRIPTION:
+
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, 
+// which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+//     // ***STARTER CODE***
+
+//  function howManyLightsabersDoYouOwn(name) {
+//   return
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require("chai")
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual( howManyLightsabersDoYouOwn(), 0 )
+//     assert.strictEqual( howManyLightsabersDoYouOwn("Adam"), 0 )
+//     assert.strictEqual( howManyLightsabersDoYouOwn("Zach"), 18 )
+//     })
+// })
+
+    // ******MY ANSWER********
+
+function howManyLightsabersDoYouOwn(name='anyone else') {
+  return name.toLowerCase()==='zach'?18:0
+}
