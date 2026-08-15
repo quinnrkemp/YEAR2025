8a706040aef0c56f5d2b320691be2db942535112
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-08-14 23:43:10 -0400

    coding chall for 081426

---
+++ 081426.js
@@ -0,0 +1,29 @@
+    // CHALLENGE NAME-  Parse float
+
+    // DESCRIPTION:
+
+// Write function parseF which takes an input and returns a number or 
+// null if conversion is not possible. The input can be one of many 
+// different types so be aware.
+
+//     // ***STARTER CODE***
+
+//   function parseF(s) {
+  
+// }  
+
+//     //   ******TEST CASES*****
+    
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(parseF("1"), 1.0);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function parseF(s) {
+  return parseFloat(s) || NaN
+}
