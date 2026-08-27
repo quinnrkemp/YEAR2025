2209b0e444d18c78fb1174b2bec8bbba85c0f997
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-08-27 00:43:05 -0400

    coding chall for 082726

---
+++ 082726.js
@@ -0,0 +1,43 @@
+    // CHALLENGE NAME-  Remove duplicates from list
+
+    // DESCRIPTION:
+
+// Define a function that removes duplicates from an array of non 
+// negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+//     // ***STARTER CODE***
+
+//  function distinct(a) {
+//   return [];
+// }   
+
+//     //   ******TEST CASES*****
+    
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Sample Tests", () => {
+//   it("test", () => {
+//     assert.deepEqual(distinct([1]), [1]);
+//     assert.deepEqual(distinct([1,2]), [1,2]);
+//     assert.deepEqual(distinct([1,1,2]), [1,2]);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function distinct(a) {
+ let res= new Set(a)
+ return Array.from(res)
+  
+// ***this is my answer***  
+}
