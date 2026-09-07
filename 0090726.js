f971aa186f12704ee8b79e815bf44aac079c4fca
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-09-07 18:39:06 -0400

    coding chall for 090726

---
+++ 0090726.js
@@ -0,0 +1,11 @@
+    // ******MY ANSWER********  (duplicate submit from yesterday.  previously pushed to the wrong date)
+
+function getDrinkByProfession(param){
+let p=param.toLowerCase()
+ return p ==='jabroni'?"Patron Tequila":
+ p ==='school counselor'?"Anything with Alcohol":
+ p ==='programmer'?"Hipster Craft Beer":
+ p ==='bike gang member'?"Moonshine":
+ p ==='politician'?"Your tax dollars":
+ p ==='rapper'?"Cristal":'Beer'
+}
