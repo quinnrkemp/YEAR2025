dd34019fb6b67def298b5cb745afce053f7eff8d
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-09-12 03:08:35 -0400

    coding chall for 091226

---
+++ 091226.js
@@ -0,0 +1,41 @@
+    // CHALLENGE NAME-  For Twins: 1. Types
+
+
+    // DESCRIPTION:
+
+// This kata series was created for friends of mine who just 
+// started to learn programming. Wish you all the best and 
+// keep your mind open and sharp!
+
+// Task:
+// Write a function that will accept two parameters: variable 
+// and type and check if type of variable is matching type. 
+// Return true if types match or false if not.
+
+// Examples:
+// 42, "number"   --> true
+// "42", "number" --> false
+
+//     // ***STARTER CODE***
+
+//   function typeValidation(variable, type) {
+//   // Your code should be here ;) 
+// }  
+
+//     //   ******TEST CASES*****
+    
+// const { assert } = require('chai');
+
+// describe('Example tests', function() {
+//   it("Basic Tests", function() {
+//     assert.strictEqual(typeValidation(42, "number"), true);
+//     assert.strictEqual(typeValidation("42", "number"), false);
+//   });
+// });
+
+    // ******MY ANSWER********
+
+function typeValidation(v, t) {
+ return typeof v===t
+  // Your code should be here ;) 
+}
