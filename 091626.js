d5ebcb063d0fbf8f6c0b14b3f1ed8990ad5439f0
Quinn R Kemp <95337530+quinnrkemp@users.noreply.github.com>
2026-09-16 10:37:41 -0400

    coding chall for 091626

---
+++ 091626.js
@@ -0,0 +1,31 @@
+    // CHALLENGE NAME-  Training JS #4: Basic data types--Array
+    // practice work from 091526
+
+        // ******MY ANSWER********
+
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length-1]
+}
+function pushElement(arr){
+  var el=1;
+  arr.push(el)
+  
+  //push el to arr
+  
+  return arr
+}
+function popElement(arr){
+  arr.pop()
+  //pop an element from arr
+  
+  return arr
+}
